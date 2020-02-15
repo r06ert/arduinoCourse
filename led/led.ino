5a54f6c0d9b68aff84516dc92eded3443f112c7e
r06ert <r06ert85@wp.pl>
2020-02-15 13:59:54 +0100

    Lesson 2

--- led/led.ino
+++ led/led.ino
@@ -28,8 +28,13 @@ https://www.arduino.cc/reference/en/language/functions/digital-io/digitalwrite/
 funkcja Arduino analogWrite(pin, value)
 https://www.arduino.cc/reference/en/language/functions/analog-io/analogwrite/
 
-funkcja Arduino delay(ms)
-https://www.arduino.cc/reference/en/language/functions/time/delay/
+funkcja Arduino milis();
+https://www.arduino.cc/reference/en/language/functions/time/millis/
+
+funkcje Arduino: Serial.begin(), Serial.print(), Serial.println()
+https://www.arduino.cc/reference/en/language/functions/communication/serial/begin/
+https://www.arduino.cc/reference/en/language/functions/communication/serial/print/
+https://www.arduino.cc/reference/en/language/functions/communication/serial/println/
 
 tryby pracy PWM - teoria
 https://www.arduino.cc/en/Tutorial/PWM
@@ -39,11 +44,8 @@ https://www.arduino.cc/en/Tutorial/PWM
 wszystkie typy zmiennych uzywanych w Arduino
 https://www.arduino.cc/reference/en/#variables
 
-zmienna typu int
-https://www.arduino.cc/reference/en/language/variables/data-types/int/
-
-struktura "for" jezyka c++
-https://www.arduino.cc/reference/en/language/structure/control-structure/for/
+slowo kluczowe static jezyka c++
+https://www.arduino.cc/reference/en/language/variables/variable-scope--qualifiers/static/
 
 struktura if else jezyka c++
 https://www.arduino.cc/reference/en/language/structure/control-structure/if/
@@ -58,73 +60,128 @@ void Blink(void);
 void BlinkSmoothly(void);
 
 
-int button = 0; //stworz zmienna button i nadaj jej wartosc poczatkowa 0
+int przycisk = 0;
+
+unsigned long aktualnyCzas10 = 0;
+unsigned long zapamietanyCzas10 = 0;
+unsigned long roznicaCzasu10 = 0;
+
+unsigned long aktualnyCzas100 = 0;
+unsigned long zapamietanyCzas100 = 0;
+unsigned long roznicaCzasu100 = 0;
 
 //funkcja arduino setup() - wywolywana tylko jeden raz na poczatku programu
 void setup() {
+
+  Serial.begin(115200); //zainicjuj UART do debugoania kodu
   
   pinMode(LED, OUTPUT); //zainicuj pin portu o numerze LED jak wyjście
   
   pinMode(BUTTON, INPUT_PULLUP); //zainicuj pin portu o numerze BUTTON jako wejście z "podciąganiem"
+
+  Serial.println("Start programu!"); //wyslij wiadomosc przez UART - port do debugowania kodu
 }
 
 
 //funkcja arduino loop() - wywolywana cyklicznie
 void loop() {
 
-  button = digitalRead(BUTTON); //odczytaj stan przycisku i zapisz do zmiennej button
+  przycisk = digitalRead(BUTTON); //odczytaj stan przycisku i zapisz do zmiennej button
 
-  //struktura "if else" - tworzenie warunków w kodzie
-  if (button == HIGH ) {
-    //jeżeli zmienna button ma wartość HIGH to wykonaj kod w tych nawiasach klamrowych
-    
-    Blink(); //wywolaj funkcje Blink()
-    
-  } else {
-    //jeżeli zmienna button ma wartość inna niz HIGH (czyli ma wartosc LOW) to wykonaj kod w tych nawiasach klamrowych
-    
-    BlinkSmoothly(); //wywolaj funkcje BlinkSmoothly()
-    
+  //oblicz roznice czasu, odmierzenie 10ms 
+  aktualnyCzas10 = millis(); //Pobierz liczbe milisekund od startu programu
+  roznicaCzasu10 = aktualnyCzas10 - zapamietanyCzas10;
+
+  //oblicz roznice czasu, odmierzenie 10ms 
+  aktualnyCzas100 = millis(); //Pobierz liczbe milisekund od startu programu
+  roznicaCzasu100 = aktualnyCzas100 - zapamietanyCzas100;
+
+
+  if (roznicaCzasu10 >= 10) {
+    zapamietanyCzas10 = aktualnyCzas10; //Zapamietaj aktualny czas
+
+    //kod tutaj bedzie sie wykonywac co 10ms
+
+    //migiaj dioda plynnie lub nie, w zaleznosci od stanu przycisku
+    if (przycisk == HIGH) {
+      Blink();
+    } else {
+      BlinkSmoothly();
+    }
+
   }
-  
+
+  if (roznicaCzasu100 >= 100) {
+    //Zapamietaj aktualny czas
+    zapamietanyCzas100 = aktualnyCzas100;
+    
+    //kod tutaj bedzie sie wykonywac co 100ms
+
+    //wyslij na UART dane debugowe
+    Serial.print("zmienna przycisk ma wartosc: ");
+    Serial.println(przycisk);
+  }
 }
 
 
-//funkcja realizuje procedure jdnego okresu migania dioda LED
 void Blink(void){
-    //ustaw pin LED na wartość HIGH i odczekaj 1000ms
-    digitalWrite(LED, HIGH);
-    delay(1000);
+    //zainicjuj zmienne statyczne
+    static int blinkCnt = 0;
+    static int blinkDir = 0;
+
+    //odmierzaj liczba powtorzen i kiedy wiecej lub rowne 100 to inkeremntuj zmienna blinkDir
+    blinkCnt++;
+    if (blinkCnt>=100) {
+      blinkCnt=0;
+      blinkDir++;
+    }
     
-    //ustaw pin LED na wartość LOW i odczekaj 1000ms
-    digitalWrite(LED, LOW);
-    delay(1000);
+    if (blinkDir%2) {
+      //kod tutaj wykona sie za kazdym razem kiedy operacja blinkDir%2 da reszte rozna od 0
+
+      //ustaw pin LED na wartość HIGH i odczekaj 1000ms
+      digitalWrite(LED, HIGH);
+
+    } else {
+      //kod tutaj wykona sie za kazdym razem kiedy operacja blinkDir%2 da rowna 0
+
+      //ustaw pin LED na wartość LOW i odczekaj 1000ms
+      digitalWrite(LED, LOW);
+      
+    }
 }
 
 
 //funkcja realizuje procedure jednego okresu plynnego migania dioda LED
 void BlinkSmoothly(void) {
-  int brightnessDown = 0;
 
-  //struktura "for" - zainicjuj zmienna brightnessUp wartością zerą i inkrementuj ją o wartość 1 tak długo jak długo jej wartość nie przekracza 100
-  for (int brightnessUp = 0; brightnessUp <= 100; brightnessUp = brightnessUp+1) {
-    //kod w tym nawiasie klamrowym bedze sie tak dlugo wykonywac jak dlugo spelniony bedzie warunek (brightnessUp <= 100)
-
-    //ustaw PWM na wartosc odpowiadajacej wartosci brightness i odczekaj 10 milisekund
-    analogWrite(LED, brightnessUp);
-    delay(10);
-  }
+  //zainicjuj zmienne statyczne
+  static int blinkCnt = 0;
+  static int blinkDir = 1;
+  static int brightnrss = 0;
   
-  brightnessDown = 100;
-  //struktura "while" - powtarzaj kod tak długo jak długo wartosc zmiennej brightnessDown jest dodatnia lub rowna zero
-  while(brightnessDown >= 0){
-    //kod w tym nawiasie klamrowym bedze sie tak dlugo wykonywac jak dlugo spelniony bedzie warunek (brightnessDown >= 0)
+  if (blinkDir%2) {
+    //kod tutaj wykona sie za kazdym razem kiedy operacja blinkDir%2 da reszte rozna od 0
+    
+    //ustaw PWM na wartosc odpowiadajacej wartosci blinkCnt
+    analogWrite(LED, blinkCnt);
 
-    //ustaw PWM na wartosc odpowiadajacej wartosci brightness i odczekaj 10 milisekund
-    analogWrite(LED, brightnessDown);
-    delay(10);
+    //odmierzaj liczba powtorzen i kiedy wiecej lub rowne 100 to inkeremntuj zmienna blinkDir
+    blinkCnt++;
+    if ( blinkCnt >= 100 ) {
+      blinkDir++;
+    }
 
-    //dekrementuj zmienna brightnessDown
-    brightnessDown=brightnessDown-1;
+  } else {
+    //kod tutaj wykona sie za kazdym razem kiedy operacja blinkDir%2 da rowna 0
+    
+    //ustaw PWM na wartosc odpowiadajacej wartosci blinkCnt
+    analogWrite(LED, blinkCnt);
+
+    //odmierzaj liczba powtorzen i kiedy wiecej lub rowne 100 to inkeremntuj zmienna blinkDir
+    blinkCnt--;
+    if ( blinkCnt <= 0 ) {
+      blinkDir++;
+    }
   }
 }
